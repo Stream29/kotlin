d3953e1ed9ab4fd61ffe7fe707ba5f39965a4a2f
Aleksandr.Shefer <aleksandr.shefer@jetbrains.com>
2026-04-17 14:30:46 +0200

    [Wasm] inline suspendBody + make the code around suspend more lightweight

--- compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/BackendWasmSymbols.kt
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/BackendWasmSymbols.kt
@@ -223,7 +223,6 @@ class BackendWasmSymbols(
         val resumeThrowIntrinsic = CallableIds.resumeThrowIntrinsic.functionSymbol()
         val resumeWithIntrinsic = CallableIds.resumeWithIntrinsic.functionSymbol()
         val buildResumeIntrinsicSuspendResult = CallableIds.buildResumeIntrinsicSuspendResult.functionSymbol()
-        val buildResumeIntrinsicValueResult = CallableIds.buildResumeIntrinsicValueResult.functionSymbol()
         val resumeWasmContinuationAndReturnResult = CallableIds.resumeWasmContinuationAndReturnResult.functionSymbol()
         val coroutineImpl = "CoroutineImplStackSwitching".coroutinesClassId
 
@@ -530,7 +529,6 @@ private object CallableIds {
     val resumeThrowIntrinsic = "resumeThrowIntrinsic".wasmCallableId
     val resumeWithIntrinsic = "resumeWithIntrinsic".wasmCallableId
     val buildResumeIntrinsicSuspendResult = "buildResumeIntrinsicSuspendResult".wasmCallableId
-    val buildResumeIntrinsicValueResult = "buildResumeIntrinsicValueResult".wasmCallableId
     val resumeWasmContinuationAndReturnResult = "resumeWasmContinuationAndReturnResult".wasmCallableId
 
     val kotlinToJsStringAdapter = "kotlinToJsStringAdapter".wasmCallableId
