697c01619de406bdf0d640fb7403a9938b0d4b73
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-04-23 01:15:27 +0200

    [Analysis API] resolver: drop redundant `resolveToSymbolsCache` cache
    All implementations goes through `resolveCallCache`/`resolveSymbolCache`
    under the hood
    
    ^KT-66039

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/components/KaFirInternalCacheStorage.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/components/KaFirInternalCacheStorage.kt
@@ -5,8 +5,6 @@
 
 package org.jetbrains.kotlin.analysis.api.fir.components
 
-import com.github.benmanes.caffeine.cache.Cache
-import com.github.benmanes.caffeine.cache.Caffeine
 import com.intellij.psi.util.CachedValue
 import org.jetbrains.kotlin.analysis.api.KaSession
 import org.jetbrains.kotlin.analysis.api.components.KaSessionComponent
@@ -17,7 +15,6 @@ import org.jetbrains.kotlin.analysis.api.platform.caches.NullableCaffeineCache
 import org.jetbrains.kotlin.analysis.api.platform.caches.withStatsCounter
 import org.jetbrains.kotlin.analysis.api.resolution.KaCallResolutionAttempt
 import org.jetbrains.kotlin.analysis.api.resolution.KaSymbolResolutionAttempt
-import org.jetbrains.kotlin.analysis.api.symbols.KaSymbol
 import org.jetbrains.kotlin.analysis.low.level.api.fir.file.structure.LLFirInBlockModificationTracker
 import org.jetbrains.kotlin.analysis.low.level.api.fir.statistics.LLStatisticsService
 import org.jetbrains.kotlin.analysis.utils.caches.softCachedValue
@@ -55,14 +52,6 @@ internal class KaFirInternalCacheStorage(private val analysisSession: KaFirSessi
             NullableCaffeineCache {
                 it.withStatsCounter(statisticsService?.analysisSessions?.resolveSymbolCacheStatsCounter)
             }
-        }
-    }
-
-    val resolveToSymbolsCache: CachedValue<Cache<KaFirReference, Collection<KaSymbol>>> by lazy {
-        softCachedValueWithPsiKey {
-            Caffeine.newBuilder()
-                .withStatsCounter(statisticsService?.analysisSessions?.resolveToSymbolsCacheStatsCounter)
-                .build()
         }
     }
 
